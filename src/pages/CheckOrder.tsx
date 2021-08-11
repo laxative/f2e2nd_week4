d6f25c7f275bb85c44451393e44ed82f1f1a6299
laxative.hsieh <laxative.hsieh@yi-spaces.com>
2021-08-11 18:50:38 +0800

    Feat(CheckOrder): implement appearance

--- src/pages/CheckOrder.tsx
+++ src/pages/CheckOrder.tsx
@@ -1,5 +1,17 @@
+import HeaderBar from '../components/HeaderBar';
+import orderDownPhoto from '../assets/svg/CheckOrder/photo_order done.svg';
+
 const CheckOrder = () => {
-  return <h1>CheckOrder</h1>;
+  return (
+    <>
+      <HeaderBar />
+      <div className="check-order-wrapper">
+        <img src={orderDownPhoto} />
+        <p>訂單已完成 !</p>
+        <button>結束</button>
+      </div>
+    </>
+  );
 };
 
 export default CheckOrder;
